8583cc63acf8b7253c4790aa12230500313394e8
Giulia Paolillo <starkweissgluhend@gmail.com>
2023-03-03 13:13:27 +0100

    relinked files moved js to js link

---
+++ src/app.js
@@ -0,0 +1,11 @@
+function alertTime(event) {
+  if (event.target.value.length > 0) {
+    alert(
+      `It is ${moment()
+        .tz(event.target.value)
+        .format("dddd, MMMM D, YYYY hh:mm A")} in ${event.target.value}`
+    );
+  }
+}
+let selectElement = document.querySelector("#countries");
+selectElement.addEventListener("change", alertTime);
