1f5861123b60eff94af88024c8d6cd477e7eb67f
Giulia Paolillo <starkweissgluhend@gmail.com>
2023-03-09 13:41:13 +0100

    added the dropdown functionality

--- src/app.js
+++ src/app.js
@@ -1,56 +1,82 @@
-/* function alertTime(event) {
-  if (event.target.value.length > 0) {
-    alert(
-      `It is ${moment()
-        .tz(event.target.value)
-        .format("dddd, MMMM D, YYYY hh:mm A")} in ${event.target.value}`
-    );
-  }
-}
-let selectElement = document.querySelector("#countries");
-selectElement.addEventListener("change", alertTime); */
-
 function updateTime() {
   let dateSaoPauloElement = document.querySelector("#sao_paulo .date");
-  let currentDateSaoPaulo = moment()
-    .tz("America/Sao_Paulo")
-    .format("dddd, MMMM Do YYYY");
 
-  let currentTimeSaoPaulo = moment()
-    .tz("America/Sao_Paulo")
-    .format("hh:mm:ss [<span>]a[</span>]");
+  if (dateSaoPauloElement) {
+    let currentDateSaoPaulo = moment()
+      .tz("America/Sao_Paulo")
+      .format("dddd, MMMM Do YYYY");
 
-  let timeSaoPauloElement = document.querySelector("#sao_paulo .time");
+    let currentTimeSaoPaulo = moment()
+      .tz("America/Sao_Paulo")
+      .format("hh:mm:ss [<span>]a[</span>]");
 
-  dateSaoPauloElement.innerHTML = currentDateSaoPaulo;
-  timeSaoPauloElement.innerHTML = currentTimeSaoPaulo;
+    let timeSaoPauloElement = document.querySelector("#sao_paulo .time");
+
+    dateSaoPauloElement.innerHTML = currentDateSaoPaulo;
+    timeSaoPauloElement.innerHTML = currentTimeSaoPaulo;
+  }
 
   let dateNYElement = document.querySelector("#new-york .date");
-  let currentDateNewYork = moment()
-    .tz("America/New_York")
-    .format("dddd, MMMM Do YYYY");
 
-  let timeNewYorkElement = document.querySelector("#new-york .time");
-  let currentTimeNewYork = moment()
-    .tz("America/New_York")
-    .format("hh:mm:ss [<span>]a[</span>]");
+  if (dateNYElement) {
+    let currentDateNewYork = moment()
+      .tz("America/New_York")
+      .format("dddd, MMMM Do YYYY");
 
-  timeNewYorkElement.innerHTML = currentTimeNewYork;
-  dateNYElement.innerHTML = currentDateNewYork;
+    let timeNewYorkElement = document.querySelector("#new-york .time");
+    let currentTimeNewYork = moment()
+      .tz("America/New_York")
+      .format("hh:mm:ss [<span>]a[</span>]");
+
+    timeNewYorkElement.innerHTML = currentTimeNewYork;
+    dateNYElement.innerHTML = currentDateNewYork;
+  }
 
   let dateShanghaiElement = document.querySelector("#shanghai .date");
-  let currentDateShanghai = moment()
-    .tz("Asia/Shanghai")
-    .format("dddd, MMMM Do YYYY");
+  if (dateShanghaiElement) {
+    let currentDateShanghai = moment()
+      .tz("Asia/Shanghai")
+      .format("dddd, MMMM Do YYYY");
 
-  let timeShanghaiElement = document.querySelector("#shanghai .time");
-  let currentTimeShanghai = moment()
-    .tz("Asia/Shanghai")
-    .format("hh:mm:ss [<span>]a[</span>]");
+    let timeShanghaiElement = document.querySelector("#shanghai .time");
+    let currentTimeShanghai = moment()
+      .tz("Asia/Shanghai")
+      .format("hh:mm:ss [<span>]a[</span>]");
 
-  dateShanghaiElement.innerHTML = currentDateShanghai;
-  timeShanghaiElement.innerHTML = currentTimeShanghai;
+    dateShanghaiElement.innerHTML = currentDateShanghai;
+    timeShanghaiElement.innerHTML = currentTimeShanghai;
+  }
+}
+
+function newTimeZoneEntry(event) {
+  setInterval(function () {
+    if (event.target.value.length > 0) {
+      let timeZone = event.target.value;
+      let formattedTimeZone = timeZone
+        .replace("_", " ")
+        .replace("Sao", "São")
+        .split("/")[1];
+      let cityElement = document.querySelector("#city");
+      cityElement.innerHTML = `<div class="cities">
+          <div class="data">
+            <h2>${formattedTimeZone}</h2>
+            <i class="fa-regular fa-calendar calendar"></i>
+            <span class="date">${moment()
+              .tz(timeZone)
+              .format("dddd, MMMM Do YYYY")}</span>
+          </div>
+          <div class="time">${moment()
+            .tz(timeZone)
+            .format("hh:mm:ss")} <span>${moment()
+        .tz(timeZone)
+        .format("a")}</span></div>
+        </div>`;
+    }
+  }, 1000);
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let selectElement = document.querySelector("#countries");
+selectElement.addEventListener("change", newTimeZoneEntry);
